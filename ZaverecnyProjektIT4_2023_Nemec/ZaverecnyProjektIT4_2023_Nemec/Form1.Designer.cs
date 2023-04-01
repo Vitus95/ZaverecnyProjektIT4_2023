b29f346b4ba8257f104dc63def459e74016694ea
Vitus95 <nemec.vitek@stud.vassboskovice.cz>
2023-04-01 22:50:09 +0200

    práce

---
+++ ZaverecnyProjektIT4_2023_Nemec/ZaverecnyProjektIT4_2023_Nemec/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace ZaverecnyProjektIT4_2023_Nemec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNajdiZ = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtJmeno = new System.Windows.Forms.TextBox();
+            this.txtPrijmeni = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.dataZamestnanci = new System.Windows.Forms.DataGridView();
+            this.btnVymazZ = new System.Windows.Forms.Button();
+            this.btnEditujZ = new System.Windows.Forms.Button();
+            this.btnPridatZ = new System.Windows.Forms.Button();
+            this.txtNarozeni = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listUser = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataZamestnanci)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtID.Location = new System.Drawing.Point(198, 90);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(150, 30);
+            this.txtID.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(12, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 25);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Číslo Zaměstnance";
+            // 
+            // btnNajdiZ
+            // 
+            this.btnNajdiZ.Location = new System.Drawing.Point(354, 90);
+            this.btnNajdiZ.Name = "btnNajdiZ";
+            this.btnNajdiZ.Size = new System.Drawing.Size(144, 30);
+            this.btnNajdiZ.TabIndex = 16;
+            this.btnNajdiZ.Text = "Najít";
+            this.btnNajdiZ.UseVisualStyleBackColor = true;
+            this.btnNajdiZ.Click += new System.EventHandler(this.btnNajdiZ_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(120, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 25);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Jméno";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label3.Location = new System.Drawing.Point(111, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 25);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Příjmení";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label4.Location = new System.Drawing.Point(43, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 25);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Datum narození";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label5.Location = new System.Drawing.Point(132, 237);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 25);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label6.Location = new System.Drawing.Point(114, 273);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 25);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Telefon";
+            // 
+            // txtJmeno
+            // 
+            this.txtJmeno.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtJmeno.Location = new System.Drawing.Point(198, 126);
+            this.txtJmeno.Name = "txtJmeno";
+            this.txtJmeno.Size = new System.Drawing.Size(300, 30);
+            this.txtJmeno.TabIndex = 22;
+            // 
+            // txtPrijmeni
+            // 
+            this.txtPrijmeni.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtPrijmeni.Location = new System.Drawing.Point(198, 162);
+            this.txtPrijmeni.Name = "txtPrijmeni";
+            this.txtPrijmeni.Size = new System.Drawing.Size(300, 30);
+            this.txtPrijmeni.TabIndex = 23;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtEmail.Location = new System.Drawing.Point(198, 234);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(300, 30);
+            this.txtEmail.TabIndex = 25;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtTel.Location = new System.Drawing.Point(198, 270);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(300, 30);
+            this.txtTel.TabIndex = 26;
+            // 
+            // dataZamestnanci
+            // 
+            this.dataZamestnanci.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataZamestnanci.Location = new System.Drawing.Point(505, 90);
+            this.dataZamestnanci.Name = "dataZamestnanci";
+            this.dataZamestnanci.Size = new System.Drawing.Size(651, 300);
+            this.dataZamestnanci.TabIndex = 27;
+            // 
+            // btnVymazZ
+            // 
+            this.btnVymazZ.Location = new System.Drawing.Point(403, 306);
+            this.btnVymazZ.Name = "btnVymazZ";
+            this.btnVymazZ.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnVymazZ.Size = new System.Drawing.Size(96, 84);
+            this.btnVymazZ.TabIndex = 28;
+            this.btnVymazZ.Text = "Vymazat";
+            this.btnVymazZ.UseVisualStyleBackColor = true;
+            this.btnVymazZ.Click += new System.EventHandler(this.btnVymazZ_Click);
+            // 
+            // btnEditujZ
+            // 
+            this.btnEditujZ.Location = new System.Drawing.Point(301, 306);
+            this.btnEditujZ.Name = "btnEditujZ";
+            this.btnEditujZ.Size = new System.Drawing.Size(96, 84);
+            this.btnEditujZ.TabIndex = 29;
+            this.btnEditujZ.Text = "Edituj";
+            this.btnEditujZ.UseVisualStyleBackColor = true;
+            this.btnEditujZ.Click += new System.EventHandler(this.btnEditujZ_Click);
+            // 
+            // btnPridatZ
+            // 
+            this.btnPridatZ.Location = new System.Drawing.Point(198, 306);
+            this.btnPridatZ.Name = "btnPridatZ";
+            this.btnPridatZ.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnPridatZ.Size = new System.Drawing.Size(96, 84);
+            this.btnPridatZ.TabIndex = 30;
+            this.btnPridatZ.Text = "Přidat";
+            this.btnPridatZ.UseVisualStyleBackColor = true;
+            this.btnPridatZ.Click += new System.EventHandler(this.btnPridatZ_Click);
+            // 
+            // txtNarozeni
+            // 
+            this.txtNarozeni.Location = new System.Drawing.Point(197, 204);
+            this.txtNarozeni.Name = "txtNarozeni";
+            this.txtNarozeni.Size = new System.Drawing.Size(301, 20);
+            this.txtNarozeni.TabIndex = 31;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Wide Latin", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(75, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(1117, 65);
+            this.label7.TabIndex = 32;
+            this.label7.Text = "ARBEIT MACHT FREI";
+            // 
+            // listUser
+            // 
+            this.listUser.HideSelection = false;
+            this.listUser.Location = new System.Drawing.Point(197, 417);
+            this.listUser.Name = "listUser";
+            this.listUser.Size = new System.Drawing.Size(301, 182);
+            this.listUser.TabIndex = 33;
+            this.listUser.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1234, 611);
+            this.Controls.Add(this.listUser);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtNarozeni);
+            this.Controls.Add(this.btnPridatZ);
+            this.Controls.Add(this.btnEditujZ);
+            this.Controls.Add(this.btnVymazZ);
+            this.Controls.Add(this.dataZamestnanci);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPrijmeni);
+            this.Controls.Add(this.txtJmeno);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnNajdiZ);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataZamestnanci)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNajdiZ;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtJmeno;
+        private System.Windows.Forms.TextBox txtPrijmeni;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.DataGridView dataZamestnanci;
+        private System.Windows.Forms.Button btnVymazZ;
+        private System.Windows.Forms.Button btnEditujZ;
+        private System.Windows.Forms.Button btnPridatZ;
+        private System.Windows.Forms.DateTimePicker txtNarozeni;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListView listUser;
+    }
+}
+
---
+++ ZaverecnyProjektIT4_2023_Nemec/ZaverecnyProjektIT4_2023_Nemec/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace ZaverecnyProjektIT4_2023_Nemec
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        SqlDataReader dr;
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Employees;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            con.Open ();
+            GetAllEmployeeRecord();
+        }
+
+        private void GetAllEmployeeRecord()
+        {
+            cmd = new SqlCommand ("Select * FROM Zamestnanci",con);
+            da = new SqlDataAdapter (cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataZamestnanci.DataSource = dt;
+
+                
+        }
+        private void btnPridatZ_Click(object sender, EventArgs e)
+        {
+            if(txtID.Text != "") 
+            {
+            SaveInfo(); 
+            }
+            else
+            {
+                MessageBox.Show("ID nemá být prázdné.");
+                txtID.Focus();
+            }
+            GetAllEmployeeRecord ();
+        }
+
+        protected void SaveInfo()
+        {
+            string QUERY = "INSERT INTO Zamestnanci " +
+            "(Id, Jmeno, Prijmeni, Narozeni, Email, Tel) " +
+            "VALUES (@Id, @Jmeno, @Prijmeni, @Narozeni, @Email, @Tel)";
+
+            SqlCommand CMD = new SqlCommand(QUERY,con);
+            CMD.Parameters.AddWithValue("@Id", txtID.Text);
+            CMD.Parameters.AddWithValue("@Jmeno", txtJmeno.Text);
+            CMD.Parameters.AddWithValue("@Prijmeni", txtPrijmeni.Text);
+            CMD.Parameters.AddWithValue("@Narozeni", txtNarozeni.Value);
+            CMD.Parameters.AddWithValue("@Email", txtEmail.Text);
+            CMD.Parameters.AddWithValue("@Tel", txtTel.Text);
+            CMD.ExecuteNonQuery();
+        }
+        protected void UpdateInfo()
+        {
+            string QUERY = "Update Zamestnanci " +
+            "Set Jmeno = @Jmeno, Prijmeni = @Prijmeni, Narozeni = @Narozeni, Email = @Email, Tel = @Tel " +
+            "where Id = @Id";
+
+            SqlCommand CMD = new SqlCommand(QUERY, con);
+            CMD.Parameters.AddWithValue("@Id", txtID.Text);
+            CMD.Parameters.AddWithValue("@Jmeno", txtJmeno.Text);
+            CMD.Parameters.AddWithValue("@Prijmeni", txtPrijmeni.Text);
+            CMD.Parameters.AddWithValue("@Narozeni", txtNarozeni.Value);
+            CMD.Parameters.AddWithValue("@Email", txtEmail.Text);
+            CMD.Parameters.AddWithValue("@Tel", txtTel.Text);
+            CMD.ExecuteNonQuery();
+        }
+        protected void DeleteInfo()
+        {
+            string QUERY = "Delete from Zamestnanci " +
+            "where Id = @Id";
+
+            SqlCommand CMD = new SqlCommand(QUERY, con);
+            CMD.Parameters.AddWithValue("@Id", txtID.Text);
+            CMD.ExecuteNonQuery();
+        }
+
+        private void btnNajdiZ_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand("Select * FROM Zamestnanci where ID =" + txtID.Text, con);
+            da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataZamestnanci.DataSource = dt;
+
+            btnEditujZ.Visible = true;
+            btnVymazZ.Visible = true;
+
+        }
+
+        private void btnEditujZ_Click(object sender, EventArgs e)
+        {
+            UpdateInfo();
+            GetAllEmployeeRecord();
+        }
+
+        private void btnVymazZ_Click(object sender, EventArgs e)
+        {
+            DeleteInfo();
+            GetAllEmployeeRecord();
+        }
+        private void GetAllUserRecord()
+        {
+            cmd = new SqlCommand("Select * FROM Zamestnanci", con);
+            da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataZamestnanci.DataSource = dt;
+
+
+        }
+    }
+}
